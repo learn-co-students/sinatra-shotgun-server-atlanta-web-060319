3514e71f23d733c5c1f57b2c9f792eedfd3a17a3
MaryJay787 <josephmaryam.mj@gmail.com>
2019-06-23 16:04:14 -0400

    Learninf rackup vs. shotgun

--- app.rb
+++ app.rb
@@ -2,8 +2,9 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "Welcome to your app!!!! Still not my app but
+    I dont have to control c my way out of life!"
   end
 
 end
